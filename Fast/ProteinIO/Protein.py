a897fa35d7640578e1e1988e6cd1ecf2083076d9
Shane Oh <ohshane71@gmail.com>
2021-06-27 14:39:58 +0900

    added __iter__ __next__ in Protein

--- Fast/ProteinIO/Protein.py
+++ Fast/ProteinIO/Protein.py
@@ -1,11 +1,41 @@
+default_k = 8
+
 class Protein:
-    def __init__(self, id='', families='', sequence=''):
+    def __init__(self, id='', families='', sequence='', k=default_k):
         self.id = id
         self.families = families
         self.sequence = sequence
+        self.sequence_len = len(sequence)
+        self.k = int(k)
         
     def to_list(self):
         return [self.id, self.families, self.sequence]
+
+    def get_word(self, k=default_k):
+        pass
+
+    def __iter__(self):
+        self.pointer = -1
+
+        if not self.sequence:
+            protein_name = None
+            if self.id:
+                protein_name = self.id
+            raise Exception(f'[ERROR] No sequence in protein ({protein_name})')
+
+        if self.k < 1:
+            raise Exception('[ERROR] Set k for k-mer')
+
+        return self
+
+    def __next__(self):
+        if self.pointer + self.k > self.sequence_len - 1:
+            raise StopIteration
+
+        self.pointer += 1
+        
+        return self.pointer, self.sequence[self.pointer : self.pointer+self.k]
+
         
     def __repr__(self):
         return f'''\r{self.id}
